fd1a84326232964056e29ded56641ef0503c89ca
swarnimvarchaswa <2021031178@mmmut.ac.in>
2023-10-30 03:38:30 +0530

    commit

--- src/components/personal.js
+++ src/components/personal.js
@@ -3,7 +3,25 @@ import React from "react";
 export default function Personal() {
     return(
         <div>
-            
+           <div className="h-screen w-screen bg-slate-200">
+      {/* header */}
+      <div className="grid items-center justify-center w-screen bg-red-800 pt-3 pb-3 gap-1 ">
+        <p className=" text-white text-center text-3xl">
+          Other State Scholarship Portal
+        </p>
+        <p className="text-gray-200 text-sm text-center">
+          For Students Studing in other State University
+        </p>
+        {/* footer */}
+      </div>
+      <div className="w-screen bg-blue-800 fixed bottom-0 py-4">
+        <p className="text-center text-white">@ Team Garuda 2.0</p>
+      </div>
+      {/* body */}
+      <div className="px-10 py-10">
+        <p className="text-4xl text-gray-500">Student Details</p>
+      </div>
+      </div>
         </div>
     )
 }
